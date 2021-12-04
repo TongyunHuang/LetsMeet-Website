63c6efe96cfc6f126b70f07b4a45cd939b4d9e8c
TongyunHuang <tongyun2@illinois.edu>
2021-12-03 23:21:23 -0600

    handle gitignore

--- api/server.js
+++ api/server.js
@@ -5,3 +5,9 @@ const port = process.env.Port || 4000
 app.get('/',(req,res) =>{
     res.send('Hello World!')
 })
+
+
+
+// Start the server
+app.listen(port);
+console.log('Server running on port ' + port);
