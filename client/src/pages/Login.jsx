52034b56a3b6a5da0cac234982e753faeaec7f11
Hongru Wang <hongru2@illinois.edu>
2021-12-06 23:07:53 +0800

    Login textfield added

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import { Navigate } from "react-router-dom";
 import "./Login.css";
-
+import TextField from "@mui/material/TextField";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
 export default function Login() {
   // React States
   const [errorMessages, setErrorMessages] = useState({});
@@ -22,10 +25,10 @@ export default function Login() {
       password: "admin",
     },
   ];
-
+  const theme = createTheme();
   const errors = {
-    uname: "invalid username",
-    pass: "invalid password",
+    uname: "Invalid Username",
+    pass: "Invalid Password",
   };
 
   const handleSubmit = (event) => {
@@ -59,23 +62,34 @@ export default function Login() {
 
   // login form
   const renderForm = (
-    <div className="form">
-      <form onSubmit={handleSubmit}>
-        <div className="input-container">
-          <label>Username </label>
-          <input type="text" name="uname" required />
-          {renderErrorMessage("uname")}
-        </div>
-        <div className="input-container">
-          <label>Password </label>
-          <input type="password" name="pass" required />
-          {renderErrorMessage("pass")}
-        </div>
-        <div className="button-container">
-          <input type="submit" />
-        </div>
-      </form>
-    </div>
+    // <ThemeProvider theme={theme}>
+    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+      <div className="input-container">
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          label="Name"
+          name="uname"
+          autoFocus
+        />
+        {renderErrorMessage("uname")}
+
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="pass"
+          label="Password"
+          type="password"
+        />
+        {renderErrorMessage("pass")}
+      </div>
+      <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2 }}>
+        Sign In
+      </Button>
+    </Box>
+    // </ThemeProvider>
   );
   const successfulLogin = <Navigate to="/home" />;
 
